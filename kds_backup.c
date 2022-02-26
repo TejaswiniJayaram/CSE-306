65d54c77080266ad12cb6515eb2f447d4078cfc6
Tejaswini <tejuj536@gmail.com>
2022-02-26 01:16:32 +0000

    final

--- kds_backup.c
+++ kds_backup.c
@@ -30,11 +30,12 @@ int linked_list(int *int_str_array, int count)
 {
 
 	int i = 0;
-	//int num = 0;
 	struct k_linked_list *nodes, *current_node, *next_node;
 
 	LIST_HEAD(head_list);
 
+
+	printk(KERN_INFO "Inserting elements into linked list\n");
 	while(i < count)
 	{
 		nodes = kmalloc(sizeof(*nodes), GFP_KERNEL);
@@ -55,10 +56,10 @@ int linked_list(int *int_str_array, int count)
 
 
 	/* destruct linked list and free its content */
-	printk(KERN_INFO "Removing nodes from linked list");
+	printk(KERN_INFO "Removing nodes from linked list and free its content");
 	list_for_each_entry_safe(current_node, next_node, &head_list, list)
 	{
-		printk(KERN_INFO "Removing node from linked list: %d\n", current_node->data);
+		printk(KERN_INFO "Removing node: %d\n", current_node->data);
 		list_del(&current_node->list);
 		kfree(current_node);
 	}
@@ -88,15 +89,17 @@ int hash_table(int *int_str_array, int count)
 	unsigned bucket;
 
 	
+	printk(KERN_INFO "Inserting elements into hash table\n");
 	while (i < count)
 	{
 
 		nodes = (struct hash_table*)kmalloc(sizeof(struct hash_table), GFP_KERNEL);
 		if(!nodes)
 		{
-			printk(KERN_INFO "memory not allocated\n");
+			printk(KERN_INFO "Memory is not allocated\n");
 			return -ENOMEM;
 		}
+
 		nodes->data = int_str_array[i++];
 		key = nodes->data;
 		hash_add(hashtable, &nodes->hash, key);
@@ -105,7 +108,7 @@ int hash_table(int *int_str_array, int count)
 	}
 	
 	
-	printk(KERN_INFO "Iterating over a hash table and printing out elements\n");
+	printk(KERN_INFO "Iterating over a hash table\n");
 	hash_for_each(hashtable, bucket, current_node, hash)
 	{
 		printk(KERN_INFO "Hash table value is: %d\n", current_node->data);
@@ -113,7 +116,7 @@ int hash_table(int *int_str_array, int count)
 
 
 	/* look up inserted integers */
-	printk(KERN_INFO "Look up the inserted numbers & print them out\n");
+	printk(KERN_INFO "Look up the inserted numbers\n");
 	
 	for(i=0; i<count; i++)
 	{
@@ -121,7 +124,7 @@ int hash_table(int *int_str_array, int count)
 		{
 			if (current_node->data == int_str_array[i])
 			{
-				printk(KERN_INFO "Looked up data is: %d\n", current_node->data);
+				printk(KERN_INFO "Looked up number is: %d\n", current_node->data);
 			}
 		
 		}
@@ -137,7 +140,7 @@ int hash_table(int *int_str_array, int count)
 		{
 			if (current_node->data == int_str_array[i])
 			{
-				printk(KERN_INFO "Removing node from hash table : %d\n", current_node->data);
+				printk(KERN_INFO "Removing node: %d\n", current_node->data);
 				hash_del(&current_node->hash);
 				kfree(current_node);
 			}
@@ -151,7 +154,7 @@ int hash_table(int *int_str_array, int count)
 
 
 
-/* red-black trees implementation */
+/* red-black tree implementation */
 struct rb_tree_root{
 	struct rb_root root_node;
 };
@@ -183,13 +186,13 @@ void insert_rb_node(struct rb_tree_root *rb_tree_root, struct rb_tree *node)
 		}
 	}
 	
-	// insert new node 
+	/* insert new node */
 	rb_link_node(&node->nodes, parent, link);
 
-	// rebalancing the rbtree if necessary 
+	/* rebalancing the rbtree if necessary */
 	rb_insert_color(&node->nodes, &rb_tree_root->root_node);
 
-	printk(KERN_INFO "Inserted element : %d\n", node->data);
+	printk(KERN_INFO "Inserted element: %d\n", node->data);
 }
 
 
@@ -207,7 +210,7 @@ void search_rb_tree(struct rb_tree_root *rb_tree_root, struct rb_tree *node)
 
 		if (node->data == current_node->data)
 		{
-			printk(KERN_INFO "Looked up data is: %d\n", current_node->data);
+			printk(KERN_INFO "Looked up number is: %d\n", current_node->data);
 			return;
 
 		}
@@ -221,25 +224,19 @@ void search_rb_tree(struct rb_tree_root *rb_tree_root, struct rb_tree *node)
 		{
 			link = &parent->rb_right;
 		}
-		//else
-		//{
-		//	printk(KERN_INFO "Looked up data is: %d\n", current_node->data);
-		//	return;
-		//}
 
 	}
-	printk(KERN_INFO "%d not found\n", node->data);
+	printk(KERN_INFO "%d not found in red black tree\n", node->data);
 	return;
 
 }
 
 
-//deleting red bklack tree
+/* deleting nodes from red black tree */
 void remove_rb_tree(struct rb_tree_root *rb_tree_root, struct rb_tree *node)
 {
 	rb_erase(&node->nodes, &rb_tree_root->root_node);
-	//kfree(node);
-	printk(KERN_INFO "Removing node from red black tree: %d\n", node->data);
+	printk(KERN_INFO "Removing node: %d\n", node->data);
 }
 
 
@@ -253,19 +250,19 @@ int red_black_tree(int *int_str_array, int count)
 
 	if(!rb_tree_root)
 	{
-		printk("memory not allocated");
+		printk("Memory is not allocated");
 		return -ENOMEM;
 	}
 	rb_tree_root->root_node = RB_ROOT;
 
-	//insert	
+	/* insert into red black tree*/	
 	printk(KERN_INFO "Inserting elements into red black tree\n");
 	for(i = 0; i<count; i++)
 	{
 		node = (struct rb_tree *)kmalloc(sizeof(struct rb_tree), GFP_KERNEL);
 		if(!node)
 		{
-			printk("memory is not alloated !!");
+			printk("Memory is not allocated");
 			return -ENOMEM;
 		}
 
@@ -273,7 +270,7 @@ int red_black_tree(int *int_str_array, int count)
 		insert_rb_node(rb_tree_root, node);
 	}
 
-	//look up
+	/* look up for inserted elements */
 	printk(KERN_INFO "Iterating over a red black tree\n");
 	for(i = 0; i<count; i++)
 	{
@@ -282,8 +279,7 @@ int red_black_tree(int *int_str_array, int count)
 
 	}
 
-	//delete
-	
+	/* deleting nodes from red black tree */
 	printk(KERN_INFO "Removing nodes from red black tree\n");
 	for(i=0; i<count; i++)
 	{
@@ -310,25 +306,25 @@ int radix_tree(int *int_str_array, int count)
 	unsigned long value;
 	int i, number;
 
-	root_node = kmalloc(sizeof(*root_node), GFP_ATOMIC);
+	root_node = (struct radix_tree_root *)kmalloc(sizeof(struct radix_tree_root), GFP_KERNEL);
 	if(!root_node)
 	{
-		printk("memory not allocated");
+		printk("Memory is not allocated");
 		return -ENOMEM;
 	}
 
 	/* initialize a radix tree at run time */
 	INIT_RADIX_TREE(root_node, GFP_ATOMIC);
 
-	//insert
-	printk(KERN_INFO "Inserting elements into radix\n");
+	/* insert elements into radix */
+	printk(KERN_INFO "Inserting elements into radix tree\n");
 	for(i=0; i<count; i++)
 	{
 		value = int_str_array[i];
-		radix_data = kmalloc(sizeof(*radix_data), GFP_ATOMIC);
+		radix_data = (struct radix_tree_data *) kmalloc(sizeof(struct radix_tree_data), GFP_KERNEL);
 		if(!radix_data)
 		{
-			printk("memory is not allocated");
+			printk("Memory is not allocated");
 			return -ENOMEM;
 		}
 		radix_data->data = int_str_array[i];
@@ -337,51 +333,50 @@ int radix_tree(int *int_str_array, int count)
 	}
 
 	
-	//lookup
+	/* lookup for inserted elements in radix tree */
+	printk(KERN_INFO "Look up for the inserted elements in radix tree\n");
 	for(i=0; i<count; i++)
 	{
 		value = int_str_array[i];
 		radix_data = radix_tree_lookup(root_node, value);
-		printk(KERN_INFO "Looked up data is: %d\n", radix_data->data);
+		printk(KERN_INFO "Looked up number: %d\n", radix_data->data);
 	}
 	
-	//tag
-	printk(KERN_INFO "Tagging odd numbers\n");
+	/* tag all odd numbers */
+	printk(KERN_INFO "Tag odd numbers in radix tree\n");
 	for(i=0; i<count; i++)
 	{
 		value = int_str_array[i];
 		if(value % 2 != 0)
 		{
 			radix_tree_tag_set(root_node, value, 1);
-			printk(KERN_INFO "Tagged element is: %d\n", int_str_array[i]);
+			printk(KERN_INFO "Tagged element: %d\n", int_str_array[i]);
 		}
 	}
 	
-	//lookup for tagged elements
-	printk(KERN_INFO "Look up for tagged elements\n");
-	elements = kmalloc(count * sizeof(radix_data), GFP_ATOMIC);
+	/* lookup for tagged elements */
+	printk(KERN_INFO "Look up for the tagged elements in radix tree\n");
+	elements = kmalloc(count * sizeof(radix_data), GFP_KERNEL);
 	if(!elements)
 	{
-		printk("memory is not allocated");
+		printk("Memory is not allocated");
 		return -ENOMEM;
 	}
 	number = radix_tree_gang_lookup_tag(root_node, (void **)elements, 0, count, 1);
 	for(i=0; i<number; i++)
 	{
-		printk(KERN_INFO "Tagged element is: %d\n", elements[i]->data);
+		printk(KERN_INFO "Tagged element: %d\n", elements[i]->data);
 	}
 	
-	//remove all nodes in radix tree
+	/* remove all nodes in radix tree */
 	printk(KERN_INFO "Remove all inserted numbers in the radix\n");
 	for(i=0; i<count;i++)
 	{
 		value = int_str_array[i];
 		radix_tree_delete(root_node, value);
-		//kfree(root_node);
-		printk(KERN_INFO "Removed node from radix tree: %d\n", int_str_array[i]);
+		printk(KERN_INFO "Removed node: %d\n", int_str_array[i]);
 	}
 	
-
 	return 0;
 
 }
@@ -399,10 +394,10 @@ int xarray(int *int_str_array, int count)
 	unsigned long value;
 	int i;
 
-	array = kmalloc(sizeof(*array), GFP_ATOMIC);
+	array = (struct xarray *)kmalloc(sizeof(struct xarray), GFP_KERNEL);
 	if(!array)
 	{
-		printk("memory not allocated");
+		printk("Memory not allocated");
 		return -ENOMEM;
 	}
 
@@ -414,56 +409,55 @@ int xarray(int *int_str_array, int count)
 	for(i=0; i<count; i++)
 	{
 		value = int_str_array[i];
-		xarr_data = kmalloc(sizeof(*xarr_data), GFP_ATOMIC);
+		xarr_data = (struct xarray_data *)kmalloc(sizeof(struct xarray_data), GFP_KERNEL);
 		if(!xarr_data)
 		{
-			printk("memory not allocated");
+			printk("Memory is not allocated");
 			return -ENOMEM;
 		}
 		xarr_data->data = int_str_array[i];
 		xa_err(xa_store(array, value, xarr_data, GFP_ATOMIC));
-		printk(KERN_INFO "Inserted element is: %d\n", int_str_array[i]);
+		printk(KERN_INFO "Inserted element: %d\n", int_str_array[i]);
 	}
 
 
 	/*lookup for inserted elements */
-	printk(KERN_INFO "Look up the inserted numbers\n");
+	printk(KERN_INFO "Look up for the inserted numbers\n");
 	for(i=0; i<count; i++)
 	{
 		value = int_str_array[i];
 		xarr_data = xa_load(array, value);
-		printk(KERN_INFO "Looked up data is: %d\n", xarr_data->data);
+		printk(KERN_INFO "Looked up number: %d\n", xarr_data->data);
 	}
 
 
-	//tag odd elements
-	printk(KERN_INFO "Tag\n");
+	/* tag odd elements */
+	printk(KERN_INFO "Tagging odd numbers in the Xarray\n");
 	for(i=0; i<count; i++)
 	{
 		value = int_str_array[i];
 		if(value % 2 != 0)
 		{
 			xa_set_mark(array, value, XA_MARK_1);
-			printk(KERN_INFO "Tagging odd element: %d\n", int_str_array[i]);
+			printk(KERN_INFO "Tagging element: %d\n", int_str_array[i]);
 		}
 	}
 
 	
-	//lookup for tagged elements
-	printk(KERN_INFO "Tagged Xarray elements\n");
+	/* lookup for tagged elements */
+	printk(KERN_INFO "Lookup for the tagged elements in Xarray\n");
 	xa_for_each_marked(array, value, xarr_data, XA_MARK_1)
 	{
 		printk(KERN_INFO "Tagged element: %d\n", xarr_data->data);
 	}
 
 
-	//remove all nodes in xarray
+	/* remove all nodes in xarray */
 	printk(KERN_INFO "Removing elements from Xarray\n");
 	for(i=0; i<count;i++)
 	{
 		value = int_str_array[i];
 		xa_erase(array, value);
-		//kfree(array);
 		printk(KERN_INFO "Removed element: %d\n", int_str_array[i]);
 	}
 	
@@ -471,7 +465,7 @@ int xarray(int *int_str_array, int count)
 	return 0;
 }
 
-// declare a bitmap
+/* declare a bitmap */
 DECLARE_BITMAP(bitmap, 1000);
 
 int bit_map(int *int_str_array, int count)
@@ -480,27 +474,31 @@ int bit_map(int *int_str_array, int count)
 	int i, bit;
 
 
-	//set bits
+	/* set bits */
+	printk(KERN_INFO "Inserting elements into bitmap\n");
 	for(i=0; i<count; i++)
 	{
 		bit_set = int_str_array[i];
 		set_bit(bit_set, (volatile unsigned long*) &bitmap);
-		printk(KERN_INFO "Set bit in bitmap - %d\n", int_str_array[i]);
+		printk(KERN_INFO "set bit: %d\n", int_str_array[i]);
 
 	}
 
-	//printing bits
+	/* printing bits */
+	printk(KERN_INFO "printing all the set bits in bitmap\n");
+
 	for_each_set_bit(bit, (const unsigned long*) &bitmap, 1000)
 	{
-		printk(KERN_INFO "bit value in bitmap - %d\n", bit);
+		printk(KERN_INFO "bit value: %d\n", bit);
 	}
 
-	//clearing bits
+	/* clearing bits */
+	printk(KERN_INFO "clearning all bits in the bitmap\n");
 	for (i=0; i<count; i++)
 	{
 		bit_clear = int_str_array[i];
 		clear_bit(bit_clear, (volatile unsigned long*) &bitmap);
-		printk(KERN_INFO "Cleared bit in bitmap - %d\n", int_str_array[i]);
+		printk(KERN_INFO "cleared bit: %d\n", int_str_array[i]);
 	}
 
 
@@ -532,12 +530,12 @@ static int __init kds_init(void)
 		k++;
 	}
 	count = count + 1;
-	printk("count is %d\n", count);
+	printk("Number of integesr are: %d\n", count);
 
 	int_str_array = kmalloc(count * sizeof(int), GFP_KERNEL);
 	if(!int_str_array)
 	{
-		printk(KERN_INFO "memory not allocated\n");
+		printk(KERN_INFO "Memory is not allocated\n");
 		return -ENOMEM;
 	}
 			
@@ -553,25 +551,35 @@ static int __init kds_init(void)
 		else
 		{
 
-			//printk("before nodes\n");
 			int_str_array[l] = num;
 			l++;
 		
-			printk("number : %d\n", num);
+			printk("Input number is: %d\n", num);
 			num = 0;
 		}
-		//printk("char : %c\n", string_param[i]);
 		i++;
 
 	}
+
 	int_str_array[l] = num;
+	printk("Input number is: %d\n", num);
 
-
+	printk(KERN_INFO "Linked list implementation\n");
 	linked_list(int_str_array, count);
+	
+	printk(KERN_INFO "Hash table implementation\n");
 	hash_table(int_str_array, count);
+
+	printk(KERN_INFO "Red black tree implementation\n");
 	red_black_tree(int_str_array, count);
+	
+	printk(KERN_INFO "Radix tree implementation\n");
 	radix_tree(int_str_array, count);
+	
+	printk(KERN_INFO "Xarray implementation\n");
 	xarray(int_str_array, count);
+	
+	printk(KERN_INFO "Bit map implementation\n");
 	bit_map(int_str_array, count);
 
 	kfree(int_str_array);
